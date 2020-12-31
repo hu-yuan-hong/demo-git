89b9ca702025e28f8279ef6b552eec85b264002c
ls <2590017563@qq.com>
2020-12-31 16:38:58 +0800

    hello world 11

--- src/main/java/cn/kgc/Git.java
+++ src/main/java/cn/kgc/Git.java
@@ -2,10 +2,11 @@ package cn.kgc;
 
 public class Git {
     public static void main(String[] args) {
-        System.out.printf("123");
-        System.out.println("true = " + true);
-        System.out.printf("34545345");
-        System.out.println("hello world");
-        System.out.printf("787558");
+        System.out.printf("111111111");
+        System.out.printf("222222222");
+        System.out.printf("333333333");
+        System.out.printf("444444444");
+        System.out.printf("555555555");
+
     }
 }
